c1992b9120ab36c899cf7b2779026550a9e38508
Alla <a.bondarenko@gmarkg.com>
2023-06-08 13:43:57 +0300

    add

---
+++ dist/main.js
@@ -0,0 +1,16 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+// import './styles/index.scss'
+// Create heading node
+// const heading = document.createElement('h1')
+// heading.textContent = 'Interesting!'
+
+// Append heading node to the DOM
+// const app = document.querySelector('#root')
+// app.append(heading)
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0Esc0IiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90ZXN0Ly4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbIi8vIGltcG9ydCAnLi9zdHlsZXMvaW5kZXguc2NzcydcclxuLy8gQ3JlYXRlIGhlYWRpbmcgbm9kZVxyXG4vLyBjb25zdCBoZWFkaW5nID0gZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgnaDEnKVxyXG4vLyBoZWFkaW5nLnRleHRDb250ZW50ID0gJ0ludGVyZXN0aW5nISdcclxuXHJcbi8vIEFwcGVuZCBoZWFkaW5nIG5vZGUgdG8gdGhlIERPTVxyXG4vLyBjb25zdCBhcHAgPSBkb2N1bWVudC5xdWVyeVNlbGVjdG9yKCcjcm9vdCcpXHJcbi8vIGFwcC5hcHBlbmQoaGVhZGluZykiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
